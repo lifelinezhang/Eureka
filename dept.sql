95b63b1836b596108eb77183227fb0bdb9ae2505
lifeline张 <38799648+lifelinezhang@users.noreply.github.com>
2020-03-04 18:56:42 +0800

    Update dept.sql

--- dept.sql
+++ dept.sql
@@ -1,25 +1,5 @@
-/*
- Navicat Premium Data Transfer
 
- Source Server         : mysql
- Source Server Type    : MySQL
- Source Server Version : 80015
- Source Host           : localhost:3306
- Source Schema         : db01
-
- Target Server Type    : MySQL
- Target Server Version : 80015
- File Encoding         : 65001
-
- Date: 03/03/2020 23:12:04
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for dept
--- ----------------------------
+# db01
 DROP TABLE IF EXISTS `dept`;
 CREATE TABLE `dept`  (
   `deptno` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -38,3 +18,45 @@ INSERT INTO `dept` VALUES (4, '市场部', 'db01');
 INSERT INTO `dept` VALUES (5, '运维部', 'db01');
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+#db02
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept`  (
+  `deptno` bigint(20) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `db_source` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`deptno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (1, '开发部', 'db02');
+INSERT INTO `dept` VALUES (2, '人事部', 'db02');
+INSERT INTO `dept` VALUES (3, '财务部', 'db02');
+INSERT INTO `dept` VALUES (4, '市场部', 'db02');
+INSERT INTO `dept` VALUES (5, '运维部', 'db02');
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+# db03
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept`  (
+  `deptno` bigint(20) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `db_source` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`deptno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (1, '开发部', 'db03');
+INSERT INTO `dept` VALUES (2, '人事部', 'db03');
+INSERT INTO `dept` VALUES (3, '财务部', 'db03');
+INSERT INTO `dept` VALUES (4, '市场部', 'db03');
+INSERT INTO `dept` VALUES (5, '运维部', 'db03');
+
+SET FOREIGN_KEY_CHECKS = 1;
+
